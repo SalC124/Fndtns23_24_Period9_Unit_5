b19c88f56906de8a4db920249ebf9a7b61badbd3
SalC124 <saltcal124@gmail.com>
2024-03-18 15:41:45 -0400

    Day 1 = Slay 💅

--- Day1_Sample_Program.java
+++ Day1_Sample_Program.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Day1_Sample_Program {
-    
+    public static void main(String[] Args) {
+        try (Scanner input = new Scanner(System.in)) {
+            ArrayList<Integer> nums = new ArrayList<>();
+            int num = input.nextInt();
+            while (num != 0) {
+                nums.add(num);
+                System.out.println(nums);
+                num = input.nextInt();
+            }
+            nums.add(0);
+            for(int repetitions = nums.size(); repetitions > 1; repetitions--) {
+                nums.add(repetitions - 1);
+            }
+            System.out.println(nums);
+        }
+    }
 }
